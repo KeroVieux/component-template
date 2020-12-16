c7052ad2974652919e0c40de6d0a74cb001c6457
小黄狗 <308680@gmail.com>
2020-12-16 18:52:46 +0000

    Update Component.svelte

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,1 +1,6 @@
-<!-- your code here -->
+<ul>
+    <li><a href="http://hidoge.cn">1</a></li>
+    <li><a href="http://hidoge.cn">2</a></li>
+    <li><a href="http://hidoge.cn">3</a></li>
+    <li><a href="http://hidoge.cn">4</a></li>
+  </ul>
